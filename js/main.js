2f1353a33d7174aa51000d4b5ab16f940c8a7e19
Diego Orellana <diego.orellana.git@gmail.com>
2023-10-08 19:51:06 -0300

    finished first version of main section

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
+const navMenu = document.querySelector('.nav__list');
 const toggleButton = document.querySelector('.mobile__nav__toggle');
-const navMenu = document.querySelector('.nav__menu');
 
 toggleButton.addEventListener('click', () => {
-    navMenu.classList.toggle('hide__menu')
+    const visibility = navMenu.getAttribute('data-visible');
+
+    if(visibility === "false"){
+        navMenu.setAttribute('data-visible', true); //show menu on mobile
+        toggleButton.setAttribute('aria-expanded',true) //search purpose
+        toggleButton.firstElementChild.classList.replace('ri-menu-line','ri-close-line') //change icon
+    }else{
+        navMenu.setAttribute('data-visible', false)
+        toggleButton.setAttribute('aria-expanded',false)
+        toggleButton.firstElementChild.classList.replace('ri-close-line','ri-menu-line')
+    }
 })
